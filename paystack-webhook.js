277a7585b3a14cd965ae028a9e337c36d87a1f00
Epaynex <epaynexinc@gmail.com>
2025-09-06 14:20:36 +0000

    Update paystack-webhook.js

--- paystack-webhook.js
+++ paystack-webhook.js
@@ -1,3 +1,4 @@
+const { sendOnboardingEmail } = require('./mailer');
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -6,7 +7,8 @@ app.post('/api/paystack-webhook', (req, res) => {
   const event = req.body;
 
   if (event.event === 'charge.success') {
-    const email = event.data.customer.email;
+   sendOnboardingEmail(email);
+ const email = event.data.customer.email;
     const amount = event.data.amount / 100;
     const currency = event.data.currency;
 
